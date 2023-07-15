d0f0584ce38eab07c4d0e4147038fffc4928baea
JesseGuSH <gcl522@hotmail.com>
2023-07-15 16:16:58 +1000

    update form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -23,36 +23,47 @@ function Form() {
 
   return (
     <div id="container">
-      <div id="tabs">
-        <ul id="myTab">
-          <li class="active">
-            <button
-              className="inline"
-              onClick={() => toggleLogin()}
-              data-toggle="tab"
-            >
-              Login
-            </button>
-          </li>
-          <li>
-            <button
-              className="inline"
-              onClick={() => toggleRegister()}
-              data-toggle="tab"
-            >
-              Register
-            </button>
-          </li>
-        </ul>
+      <div id="tabs" className="mb-8 mt-6">
+        <button
+          class="inline mx-8 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 rounded shadow"
+          onClick={() => toggleLogin()}
+          data-toggle="tab"
+        >
+          Login
+        </button>
+        <button
+          class="inline bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 rounded shadow"
+          onClick={() => toggleRegister()}
+          data-toggle="tab"
+        >
+          Register
+        </button>
       </div>
 
       {showLogin && (
         <div class="tab-content" id="cont">
           <div class="tab-pane active" id="signIn">
-            <input id="se" type="email" placeholder="Your Email" required />
-            <input id="sp" type="password" placeholder="Password" required />
-            <input type="submit"></input>
-            <p>Forget Password?</p>
+            <input
+              id="se"
+              type="email"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Your Email"
+              required
+            />
+            <input
+              id="sp"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <button
+              type="submit"
+              class="w-full text-center py-3 rounded bg-primary text-white hover:bg-green-dark focus:outline-none my-1"
+            >
+              Submit
+            </button>
+            <p className="mt-3 text-black">Forget Password?</p>
           </div>
         </div>
       )}
@@ -61,19 +72,32 @@ function Form() {
         <div class="tab-pane" id="register">
           <input
             className=""
+            class="block border border-grey-light w-full p-3 rounded mb-4"
             id="re"
             type="email"
             placeholder="Your Email"
             required
           />
-          <input id="rp" type="password" placeholder="Password" required />
           <input
+            class="block border border-grey-light w-full p-3 rounded mb-4"
+            id="rp"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <input
+            class="block border border-grey-light w-full p-3 rounded mb-4"
             id="rrp"
             type="password"
             placeholder="Re-enter Your Password"
             required
           />
-          <input type="submit"></input>
+          <button
+            type="submit"
+            class="w-full text-center py-3 rounded bg-primary text-white hover:bg-green-dark focus:outline-none my-1"
+          >
+            Submit
+          </button>
         </div>
       )}
     </div>
