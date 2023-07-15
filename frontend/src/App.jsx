02c4a0244020a9cf62eb2d4657dafb7780b2c839
JosephJL <HeeJLJoseph@gmail.com>
2023-07-15 14:40:09 +1000

    Added TailwindCSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Travel-Planner</h1>
+        <h1 className="text-3xl font-bold underline">Travel-Planner</h1>
         <Form />
       </div>
     </>
