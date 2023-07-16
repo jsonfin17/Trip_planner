0c8d2531c1914d97c739aa596a534d2bab523713
jsonfin17 <jfinsalyne@gmail.com>
2023-07-16 14:42:56 +1000

    submit button functional
    submit button in the preference page is functional

--- frontend/src/components/PreferenceForm.jsx
+++ frontend/src/components/PreferenceForm.jsx
@@ -4,6 +4,10 @@ import Slider from "../components/Slider";
 function PreferenceForm() {
   const activities = ["Hiking", "Swimming", "Karoke", "Run", "Walk", "Fishing"];
 
+  const handleButtonClick = () => {
+    window.location.href = 'http://localhost:5173/events'; // Replace with your desired URL
+  };
+
   return (
     <div className="place-self-center w-2/4 mt-6 px-4 py-2 text-gray-700 uppercase bg-cyan-400 rounded-2xl shadow-lg shadow-indigo-500/40">
       {activities.map((activityName, index) => {
@@ -12,6 +16,7 @@ function PreferenceForm() {
       <button
         type="submit"
         className="w-full mt-6 text-center py-3 rounded bg-cyan-800 text-white hover:bg-green-dark focus:outline-none my-1"
+        onClick={handleButtonClick}
       >
         Submit
       </button>
