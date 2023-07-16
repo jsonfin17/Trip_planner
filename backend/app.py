204fdb77e19667bff1f17483246f59a07d0758a8
Hirohamada33 <mr.smartnewton@gmail.com>
2023-07-16 10:39:48 +0800

    Update backend with activity data and functionality

--- backend/app.py
+++ backend/app.py
@@ -18,10 +18,12 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return dict['hello']
+    if not check_login():
+        return redirect(url_for('login'))
+    return 'hello'
 
 
-@app.route('/kdfjkrjljgkjg', methods=['post', 'get'])
+@app.route('/login', methods=['post', 'get'])
 def login():
     if check_login():
         return redirect(url_for('index'))
@@ -68,21 +70,55 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/preference')
+@app.route('/preference', methods = ['post', 'get'])
 def preference():
+    if not check_login():
+        return redirect(url_for('login'))
+    category = {}
+    g.db = sqlite3.connect(database)
+    results = g.db.execute('SELECT category, description FROM category' ).fetchall()
+    for row in results:
+        category[row[0]] = row[1]
     if request.method == 'POST':
         try:
             username = session.get('user_id')
-            g.db = sqlite3.connect(database)
-            results = g.db.execute('SELECT activity_name, description FROM activity' ).fetchall()
-            for row in results:
-                print(row)
-            g.db.commit()
-            return redirect(url_for('login'))
+            for item in category:
+                print(request.form[item])
+                score = request.form[item]
+                g.db.execute('INSERT INTO preference VALUES (?, ?, ?)', (username, item, score, ))
+                g.db.commit()
+                print('update preference successfully!')
+            return '<p>hello!</p>'
+            
         except Exception as e:
             print(e)
-            return redirect(url_for('register'))
-    return render_template('register.html')
+            return 'error'
+    return render_template('preference.html', data = category)
+
+
+
+@app.route('/search-friends')
+def search_friends():
+    user = session.get('user_id')
+    results = g.db.execute('SELECT username FROM user WHERE user <> ?', (user, ))
+    users = []
+    for user in results:
+        users.append(user)
+    return render_template('search.html', data = users)
+
+
+@app.route('/friends')
+def friends():
+    return 'not done yet'
+
+
+@app.route('/result')
+def result():
+    return 'hello'
+
+
+def weighting():
+    return 'hello'
 
 
 def check_login():
--- backend/scheme.py
+++ backend/scheme.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 conn = sqlite3.connect('trip.db')
 script = (
     """
@@ -6,6 +7,12 @@ script = (
     CREATE TABLE user(
         username TEXT PRIMARY KEY, 
         password TEXT NOT NULL); 
+    
+    DROP TABLE IF EXISTS category;
+    CREATE TABLE category(
+        category TEXT PRIMARY KEY, 
+        description TEXT
+    );
 
     DROP TABLE IF EXISTS activity; 
     CREATE TABLE activity(
@@ -13,18 +20,50 @@ script = (
         activity_name TEXT,
         description TEXT,
         budget INTERGER,
-        location TEXT);
+        location TEXT,
+        time FLOAT, 
+        category TEXT,
+        weather_dependency BOOL, 
+        weather TEXT,
+        FOREIGN KEY (category) REFERENCES category(category)
+        );
+
 
 
     DROP TABLE IF EXISTS preference; 
     CREATE TABLE preference(
         username TEXT NOT NULL,     
-        activity_id INTEGER NOT NULL,
-        weighting INTEGER NOT NULL CHECK ((weighting <= 100) and (weighting >= 0)),
-        FOREIGN KEY (username) REFERENCES user(username),
-        FOREIGN KEY (activity_id) REFERENCES activity(activity_id));
+        category INTEGER NOT NULL,
+        weighting INTEGER NOT NULL CHECK ((weighting <= 10) and (weighting >= 0)),
+        FOREIGN KEY (username) REFERENCES user(username));
     """
 )
 conn.executescript(script)
 conn.commit()
+
+category = dict()
+category['outdoor'] = 'description for outdoor activity'
+category['music'] = 'description for music activity'
+category['historical'] = 'description for historical activity'
+category['academic'] = 'description for academic activity'
+category['water'] = 'description for water activity'
+category['film'] = 'description for film activity'
+
+
+for item in category:
+    conn.execute("INSERT INTO category VALUES (?, ?)", (item, category[item],))
+    conn.commit()
+
+list = []
+with open('text.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+        list.append(line)
+
+for i in range(len(list)):
+    budget = random.randint(0, 1000)
+    time = random.randint(1, 5)
+    conn.execute('INSERT INTO activity VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', (i, list[i], 'placeholder', budget, 'location placeholder', time, 'outdoor', False, None, ))
+    conn.commit()
+
 conn.close()
